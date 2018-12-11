af9d1050f70d0821a44692a85e67761a309eee54
miggiegg <30985099+miggiegg@users.noreply.github.com>
2018-12-10 19:20:51 -0800

    Initial Commit

---
+++ Museo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Museo
+//
+//  Created by Miguel Garcia on 12/10/18.
+//  Copyright © 2018 Miguel Garcia Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Museo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Museo
+//
+//  Created by Miguel Garcia on 12/10/18.
+//  Copyright © 2018 Miguel Garcia Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
